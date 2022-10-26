88d636b890852aede4ecfbfe081e8c288a15973b
Lazar Stojanović <lakisd98@gmail.com>
2022-10-26 03:33:36 +0200

    Add basic update route and functionality to update a campground

--- index.js
+++ index.js
@@ -17,35 +17,47 @@ async function main() {
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
 
 // Home route
 app.get("/", (req, res) => {
     res.render("home")
 })
 
-// Index route
+// Show campgrounds route
 app.get("/campgrounds", async (req, res) => {
     const campgrounds = await Campground.find({})
     res.render("campgrounds/index", { campgrounds })
 })
 
-// Create campground get route
+// New campground get route
 app.get("/campgrounds/new", (req, res) => {
     res.render("campgrounds/new")
 })
 
-// Create campground post route
+// New campground post route
 app.post("/campgrounds", async (req, res) => {
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground.id}`)
 })
 
-// Show route
+// Show campground route
 app.get("/campgrounds/:id", async (req, res) => {
-    const id = req.params.id
-    const campground = await Campground.findById(id)
+    const campground = await Campground.findById(req.params.id)
     res.render("campgrounds/show", { campground })
+})
+
+// Edit campground route
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render("campgrounds/edit", { campground })
+})
+
+app.put("/campgrounds/:id", async (req, res) => {
+    const id = req.params.id
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    res.redirect(`/campgrounds/${id}`)
 })
 
 // Starting up app on desired port
