5986860d68a9513d0ee412b6063d6af47360bc28
Lazar Stojanović <lakisd98@gmail.com>
2022-11-28 12:04:39 +0100

    Update redirect functionality

--- app.js
+++ app.js
@@ -54,7 +54,6 @@ passport.deserializeUser(User.deserializeUser())
 
 // Flash middleware before route handlers
 app.use((req, res, next) => {
-    console.log(req.session)
     res.locals.currentUser = req.user
     res.locals.success = req.flash("success")
     res.locals.error = req.flash("error")
--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.get("/login", (req, res) => {
 })
 
 router.post("/login", passport.authenticate("local", {
-    failureFlash: true, failureRedirect: "/login"
+    failureFlash: true, failureRedirect: "/login", keepSessionInfo: true
 }), (req, res) => {
     req.flash("success", "Welcome back!")
     const redirectUrl = req.session.returnTo || "/campgrounds"
