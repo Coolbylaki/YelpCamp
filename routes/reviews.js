b6895ea3d5c844b89796b5ed1fd193a20a12a5df
Lazar Stojanović <lakisd98@gmail.com>
2022-12-07 14:49:24 +0100

    Add reviews controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,29 +1,13 @@
 const express = require("express")
 const router = express.Router({ mergeParams: true })
 const asyncWrapper = require("../utilities/asyncWrapper")
-const Review = require("../models/review")
-const Campground = require("../models/campground")
 const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware")
+const reviews = require("../controllers/reviews")
 
 // Post campground reviews
-router.post("/", isLoggedIn, validateReview, asyncWrapper(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    const review = new Review(req.body.review)
-    review.author = req.user._id
-    campground.reviews.push(review)
-    await review.save()
-    await campground.save()
-    req.flash("success", "Created new review!")
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
+router.post("/", isLoggedIn, validateReview, asyncWrapper(reviews.createReview))
 
 // Delete campground review
-router.delete("/:reviewId", isLoggedIn, isReviewAuthor, asyncWrapper(async (req, res) => {
-    const { id, reviewId } = req.params
-    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-    await Review.findByIdAndDelete(reviewId)
-    req.flash("success", "Successfully deleted review!")
-    res.redirect(`/campgrounds/${id}`)
-}))
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, asyncWrapper(reviews.deleteReview))
 
 module.exports = router
