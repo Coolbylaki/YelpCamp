94085430252772a71e7bc357646bccc262a85b0e
Lazar Stojanović <lakisd98@gmail.com>
2022-12-10 10:15:43 +0100

    Update localhost url

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const userRoutes = require("./routes/users")
 // Connect to MongoDB
 main().catch(err => console.log(err))
 async function main() {
-    await mongoose.connect("mongodb://localhost:27017/yelpCamp")
+    await mongoose.connect("mongodb://127.0.0.1:27017/yelpCamp")
     console.log("Connection open!")
 }
 
