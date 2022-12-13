a1efabdc1d290653eab28cacadfbe84750b9f376
Lazar Stojanović <lakisd98@gmail.com>
2022-12-13 21:45:54 +0100

    Add image thumbnail virtual

--- models/campground.js
+++ models/campground.js
@@ -2,15 +2,20 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const Review = require("./review")
 
+// Model image Schema
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual("thumbnail").get(function () {
+    return this.url.replace("/upload", "/upload/w_200")
+})
+
 // Modeling our Schema
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
