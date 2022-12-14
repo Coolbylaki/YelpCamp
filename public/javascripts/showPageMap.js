3eef3dfcd9f1561823cfbef09b2698502a8cfea9
Lazar Stojanović <lakisd98@gmail.com>
2022-12-14 20:21:16 +0100

    Add map marker

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,8 +1,13 @@
 // Mapbox default script for map
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 4, // starting zoom
+	container: "map", // container ID
+	style: "mapbox://styles/mapbox/streets-v12", // style URL
+	center: [-74.5, 40], // starting position [lng, lat]
+	zoom: 4, // starting zoom
 });
+
+// Create a default Marker and add it to the map.
+const marker = new mapboxgl.Marker()
+	.setLngLat([12.554729, 55.70651])
+    .addTo(map);
