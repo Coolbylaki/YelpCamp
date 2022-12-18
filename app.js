1f2dd667b3104582f143ff32d5ca4f5a6fb8dff7
Lazar Stojanović <lakisd98@gmail.com>
2022-12-18 17:24:08 +0100

    Refactor Helmet configuration

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const userRoutes = require("./routes/users");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const MongoStore = require("connect-mongo");
+const { scriptSrcUrls, styleSrcUrls, connectSrcUrls, fontSrcUrls } = require("./utilities/helmetConfig");
 
 // Express
 const app = express();
@@ -75,31 +76,6 @@ app.use(flash());
 
 // Helmet config
 app.use(helmet());
-
-const scriptSrcUrls = [
-	"https://stackpath.bootstrapcdn.com/",
-	"https://api.tiles.mapbox.com/",
-	"https://api.mapbox.com/",
-	"https://kit.fontawesome.com/",
-	"https://cdnjs.cloudflare.com/",
-	"https://cdn.jsdelivr.net",
-];
-const styleSrcUrls = [
-	"https://kit-free.fontawesome.com/",
-	"https://stackpath.bootstrapcdn.com/",
-	"https://api.mapbox.com/",
-	"https://api.tiles.mapbox.com/",
-	"https://fonts.googleapis.com/",
-	"https://use.fontawesome.com/",
-	"https://cdn.jsdelivr.net",
-];
-const connectSrcUrls = [
-	"https://api.mapbox.com/",
-	"https://a.tiles.mapbox.com/",
-	"https://b.tiles.mapbox.com/",
-	"https://events.mapbox.com/",
-];
-const fontSrcUrls = [];
 app.use(
 	helmet.contentSecurityPolicy({
 		directives: {
